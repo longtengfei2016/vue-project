7eac73e8123b2ff620ff97e2d5d026dfe94f65b9
small_winder <longtengfly@163.com>
2017-12-06 15:06:35 +0800

    Add files via upload

---
+++ src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="my-header">
+    hello world!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      meanu: [topic]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
